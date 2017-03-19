648165371895cb9e3c33b2898f695f1087a5138f
Anand Nigam <annigam01@hotmail.com>
2017-03-19 17:56:21 +0530

    all working

--- RestClient-Desktop/Form1.Designer.cs
+++ RestClient-Desktop/Form1.Designer.cs
@@ -28,20 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(29, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(418, 91);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get Data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(776, 224);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 55);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "no data";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(29, 224);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(418, 329);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(638, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(438, 91);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Get Data (Async)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1109, 568);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- RestClient-Desktop/Form1.cs
+++ RestClient-Desktop/Form1.cs
@@ -1,20 +1,76 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections;
 
 namespace RestClient_Desktop
 {
     public partial class Form1 : Form
     {
+        ArrayList AllDir = new ArrayList();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "working...";
+            label1.Text = GetDirectoryCount(@"C:\Users\annigam\");
+            
+        }
+        
+        //Step1 - write the method that does the actual work
+        private string GetDirectoryCount(string CurrentDir)
+        {
+            //listBox1.Items.Add(CurrentDir);
+            AllDir.Add(CurrentDir);
+            
+            try
+            {
+                
+                string[] AllDirs = System.IO.Directory.GetDirectories(CurrentDir);
+
+                foreach (string Dir in AllDirs)
+                {
+                    AllDir.Add(Dir);
+                    GetDirectoryCount(Dir);
+                }
+
+
+            }
+            catch (Exception)
+            {
+                
+            }
+
+            return AllDir.Count.ToString();
+            
+        }
+
+        //Step2- make a wrapper around the actual method
+        private Task<string> GetDirectoryCountAsync(string CurDir)
+        {
+            return Task.Factory.StartNew(() => GetDirectoryCount(CurDir));
+
+        }
+        
+        private int GetFileChars()
+        {
+            System.IO.StreamReader SR = System.IO.File.OpenText(@"D:\text.txt");
+            string Text = SR.ReadToEnd();
+            var Allchars = Text.ToArray<char>();
+            return Allchars.Length;
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "working";
+            var result = await GetDirectoryCountAsync(@"C:\Users\annigam\");
+            label1.Text = result;
+
         }
     }
 }
